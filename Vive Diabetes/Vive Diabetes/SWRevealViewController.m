40ac445a0f0217505f6ea18e089594474f22f7fe
DanyGZ <dani_nacho.95@hotmail.com>
2017-07-26 11:28:39 -0500

    Eliminado warning en archivo .m

--- Vive Diabetes/Vive Diabetes/SWRevealViewController.m
+++ Vive Diabetes/Vive Diabetes/SWRevealViewController.m
@@ -373,6 +373,10 @@ static CGFloat scaledValue( CGFloat v1, CGFloat min2, CGFloat max2, CGFloat min1
     void (^_completion)(void);
 }
 
+- (void)pauseInteractiveTransition {
+    // not implemented
+}
+
 
 - (id)initWithRevealController:(SWRevealViewController*)revealVC containerView:(UIView*)view fromVC:(UIViewController*)fromVC
     toVC:(UIViewController*)toVC completion:(void (^)(void))completion
